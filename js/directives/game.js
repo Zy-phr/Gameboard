d4c96f1dcfb30cce43f74dfcfeeddb2089ed843c
Ricky Sanchez <RickySanchez@rickys-imac.attlocal.net>
2019-11-23 10:54:35 -0600

    Gameboard

---
+++ js/directives/game.js
@@ -0,0 +1,12 @@
+app.directive("game", function() {
+  
+    return {
+
+        restrict: 'E',
+        scope: {
+            info: '='
+        },
+        templateUrl: 'js/directives/game.html'
+    }
+  
+})
